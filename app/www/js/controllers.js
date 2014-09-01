83f9176d0543e9beeddac5c68b7e222a2e0a74f5
Bruce Lefebvre <blefebvr@adobe.com>
2014-09-01 11:01:28 -0400

    Struggling with authentication
    checkpoint

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -3,5 +3,18 @@ angular.module('starter.controllers', [])
 .controller('BlogCtrl', function($scope) {
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function($scope, $http) {
+    $scope.formData = {};
+
+    $scope.processLogin = function() {
+        $http.post('http://localhost:8080/content/espblog/posts.list.html/j_security_check', $scope.formData, {
+            headers: {enctype:'multipart/form-data'}
+        })
+            .success(function(data) {
+                console.log("success!!" + data);
+            })
+            .error(function(data, status) {
+                console.error("error: " + data || "Request failed" );
+            });
+    };
 });
