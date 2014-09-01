33533e16752fe19b6607e3a5166ffebe3dc950c6
Bruce Lefebvre <blefebvr@adobe.com>
2014-09-01 13:24:52 -0400

    Login/logout implemented

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,10 +1,17 @@
 angular.module('starter.controllers', ['starter.services'])
 
+// Sling instance hostname
+.constant('slingHost', 'http://localhost:8080')
+
 .controller('BlogCtrl', function($scope) {
 })
 
-.controller('AccountCtrl', ['$scope', '$http', 'formDataObject', 
-    function($scope, $http, formDataObject) {
+.controller('AccountCtrl', ['$scope', '$http', 'formDataObject', 'slingHost',
+    function($scope, $http, formDataObject, slingHost) {
+
+        var LOGGED_IN_USER_ID_KEY = "loggedInUserId";
+        var LOGGED_IN_USER_PWD_KEY = "loggedInUserPassword";
+
         $scope.formData = {
             _charset_: "UTF-8",
             selectedAuthType: "form",
@@ -15,25 +22,49 @@ angular.module('starter.controllers', ['starter.services'])
         };
 
         $scope.processLogin = function() {
+            login($scope.formData);
+        };
+
+        $scope.logout = function() {
+            $http.get(slingHost + '/system/sling/logout')
+                .success(function(data, status) {
+                    console.log('Logout success');
+                    localStorage.removeItem(LOGGED_IN_USER_ID_KEY);
+                    localStorage.removeItem(LOGGED_IN_USER_PWD_KEY);
+                    $scope.currentLoggedInUser = null;
+                })
+                .error(function(data, status) {
+                    console.error('Logout failed. Status: ' + status + ', Message: ' + data);
+                });
+        };
+
+        /**
+         * Private helpers
+         */
+        var login = function(formData) {
             $http({
                     method: 'POST',
-                    url: 'http://localhost:8080/j_security_check', 
-                    data: $scope.formData,
+                    url: slingHost + '/j_security_check', 
+                    data: formData,
                     headers: {
                         'Content-Type': undefined
                     },
                     transformRequest: formDataObject
                 })
                 .success(function(data, status) {
+                    $scope.loginErrorMessage = null;
                     if (status == 200) {
-                        console.log("Login SUCCESS!");
+                        console.log('Login SUCCESS!');
+                        localStorage.setItem(LOGGED_IN_USER_ID_KEY, formData.j_username);
+                        localStorage.setItem(LOGGED_IN_USER_PWD_KEY, formData.j_password);
+                        $scope.currentLoggedInUser = formData.j_username;
                     } else {
-                        console.log("Interesting status: " + status);
+                        console.log('Interesting status: ' + status);
                     }
                 })
                 .error(function(data, status) {
-                    console.error("Login failed");
-                    console.error("Status: " + status + ", Message: " + data);
+                    console.error('Login failed. Status: ' + status + ', Message: ' + data);
+                    $scope.loginErrorMessage = data;
                 });
         };
     }
