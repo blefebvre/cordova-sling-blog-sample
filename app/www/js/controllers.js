f5172bc6465c2db7b07b78be7e4d80b4934ac14e
Bruce Lefebvre <blefebvr@adobe.com>
2014-09-04 00:45:50 -0400

    Refactored authentication to it's own factory

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,8 +1,5 @@
 angular.module('starter.controllers', ['starter.services'])
 
-// Sling instance hostname
-.constant('slingHostURI', 'http://localhost:8080')
-
 .controller('BlogCtrl', ['$scope', '$http', 'slingHostURI',
     function($scope, $http, slingHostURI) {
         // Assume we're connected until proven otherwise
@@ -36,83 +33,40 @@ angular.module('starter.controllers', ['starter.services'])
     }
 ])
 
-.controller('AccountCtrl', ['$scope', '$http', 'formDataObject', 'slingHostURI',
-    function($scope, $http, formDataObject, slingHostURI) {
-
-        var LOGGED_IN_USER_ID_KEY = "loggedInUserId";
-        var LOGGED_IN_USER_PWD_KEY = "loggedInUserPassword";
-
-        // TODO: this does not confirm if the session is still valid
-
-        // TODO: session is NOT valid; need to re-log in on resume
-
+.controller('AccountCtrl', ['$scope', '$rootScope', '$http', 'formDataObject', 'slingHostURI', 'loggedInUserIdKey', 'loggedInUserPwdKey', 'userAuthentication',
+    function($scope, $rootScope, $http, formDataObject, slingHostURI, loggedInUserIdKey, loggedInUserPwdKey, userAuthentication) {
         $scope.formData = {
-            _charset_: "UTF-8",
-            selectedAuthType: "form",
-            resource: "/",
-            j_username: "",
-            j_password: "",
-            j_validate: "true"
+            j_username: '',
+            j_password: ''
         };
 
         $scope.processLogin = function() {
-            login($scope.formData);
+            userAuthentication.login($scope.formData.j_username, $scope.formData.j_password,
+                function callback(error, username) {
+                    if (error) {
+                        $scope.loginErrorMessage = error;
+                        return;
+                    }
+
+                    $scope.loginErrorMessage = null;
+                    $rootScope.currentLoggedInUser = username;
+                });
         };
 
         $scope.logout = function() {
+
+            // TODO: move logout to userAuthentication
             $http.get(slingHostURI + '/system/sling/logout')
                 .success(function(data, status) {
                     console.log('Logout success');
-                    localStorage.removeItem(LOGGED_IN_USER_ID_KEY);
-                    localStorage.removeItem(LOGGED_IN_USER_PWD_KEY);
+                    localStorage.removeItem(loggedInUserIdKey);
+                    localStorage.removeItem(loggedInUserPwdKey);
                     $scope.currentLoggedInUser = null;
                 })
                 .error(function(data, status) {
                     console.error('Logout failed. Status: [' + status + '], Message: [' + data + '].');
                 });
         };
-
-        /**
-         * Private helpers
-         */
-        var login = function(formData) {
-            $http({
-                    method: 'POST',
-                    url: slingHostURI + '/j_security_check', 
-                    data: formData,
-                    headers: {
-                        'Content-Type': undefined
-                    },
-                    transformRequest: formDataObject
-                })
-                .success(function(data, status) {
-                    $scope.loginErrorMessage = null;
-                    if (status == 200) {
-                        console.log('Login SUCCESS for user [' + formData.j_username + ']');
-                        localStorage.setItem(LOGGED_IN_USER_ID_KEY, formData.j_username);
-                        localStorage.setItem(LOGGED_IN_USER_PWD_KEY, formData.j_password);
-                        $scope.currentLoggedInUser = formData.j_username;
-                    } else {
-                        console.log('Interesting status: ' + status);
-                    }
-                })
-                .error(function(data, status) {
-                    console.error('Login failed. Status: [' + status + '], Message: [' + data + '].');
-                    $scope.loginErrorMessage = data;
-                });
-        };
-
-        // Check if user has already authenticated
-        // TODO: do this ONLY on app resume
-        var loggedInUserId = localStorage.getItem(LOGGED_IN_USER_ID_KEY);
-        if (loggedInUserId != null) {
-            var loggedInUserPassword = localStorage.getItem(LOGGED_IN_USER_PWD_KEY);
-            $scope.formData.j_username = loggedInUserId;
-            $scope.formData.j_password = loggedInUserPassword;
-
-            // Authenticate with server
-            login($scope.formData);
-        }   
     }
 ])
 
--- app/www/js/services.js
+++ app/www/js/services.js
@@ -1,5 +1,10 @@
 angular.module('starter.services', [])
 
+// Sling instance hostname
+.constant('slingHostURI', 'http://localhost:8080')
+.constant('loggedInUserIdKey', 'loggedInUserId')
+.constant('loggedInUserPwdKey', 'loggedInUserPassword')
+
 .factory('formDataObject', function() {
     return function(data) {
         var fd = new FormData();
@@ -8,4 +13,75 @@ angular.module('starter.services', [])
         });
         return fd;
     };
-});
+})
+
+.factory('userAuthentication', ['$http', 'slingHostURI', 'formDataObject', 'loggedInUserIdKey', 'loggedInUserPwdKey',
+    function($http, slingHostURI, formDataObject, loggedInUserIdKey, loggedInUserPwdKey) {
+
+        /**
+         * Attempt to log in with the given credentials.
+         */
+        var login = function(username, password, callback) {
+
+            var formData = {
+                _charset_: "UTF-8",
+                selectedAuthType: "form",
+                resource: "/",
+                j_validate: "true",
+                j_username: username,
+                j_password: password
+            };
+            
+            $http({
+                    method: 'POST',
+                    url: slingHostURI + '/j_security_check', 
+                    data: formData,
+                    headers: {
+                        'Content-Type': undefined
+                    },
+                    transformRequest: formDataObject
+                })
+                .success(function(data, status) {
+                    if (status == 200) {
+                        console.log('Login SUCCESS for user [' + formData.j_username + ']');
+                        localStorage.setItem(loggedInUserIdKey, formData.j_username);
+                        localStorage.setItem(loggedInUserPwdKey, formData.j_password);
+
+                        // Callback with the authenticated username
+                        return callback(null, formData.j_username);
+                    } else {
+                        return callback('Login status: [' + status + ']');
+                    }
+                })
+                .error(function(data, status) {
+                    console.error('Login failed. Status: [' + status + '], Message: [' + data + '].');
+                    return callback(data);
+                });
+        };
+
+        return {
+            login: login
+        };
+    }
+])
+
+.run(['$rootScope', 'slingHostURI', 'userAuthentication', 'loggedInUserIdKey', 'loggedInUserPwdKey', 
+    function($rootScope, slingHostURI, userAuthentication, loggedInUserIdKey, loggedInUserPwdKey) {
+        // Log in if credentials are available
+        var loggedInUserId = localStorage.getItem(loggedInUserIdKey);
+        if (loggedInUserId != null) {
+            var loggedInUserPassword = localStorage.getItem(loggedInUserPwdKey);
+
+            // Authenticate with server
+            userAuthentication.login(loggedInUserId, loggedInUserPassword, 
+                function callback(error, username) {
+                    if (error) {
+                        // TODO: handle error
+                        return;
+                    }
+
+                    $rootScope.currentLoggedInUser = username;
+                });
+        }   
+    }
+]);
